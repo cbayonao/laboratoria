cca0e827b1004313c2842f27a77e78f7778882e3
cbayonao <drcamilob@live.com>
2020-04-14 18:40:10 -0500

    [Feature] Integrate with Firebase Authentication

--- laboratoria/src/Home.js
+++ laboratoria/src/Home.js
@@ -4,13 +4,18 @@ import fire from './config/Fire';
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+    fire.auth().signOut();
   }
 
   render() {
     return(
       <div className="col-md-6">
       <h1>You are home, Now ready to DO posts manager</h1>
-
+      <button class="btn btn-danger" onClick={this.logout}>Logout</button>
       </div>
     );
   }
--- laboratoria/src/Login.js
+++ laboratoria/src/Login.js
@@ -5,13 +5,28 @@ import fire from './config/Fire';
 class Login extends Component {
   constructor(props) {
     super(props);
-    /* this.login = this.login.bind(this);
-     * this.handleChange = this.handleChange.bind(this);
-     * this.signup = this.signup.bind(this); */
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.signup = this.signup.bind(this);
     this.state = {
       email: '',
       password: ''
     };
+  }
+
+  login(e) {
+    e.preventDefault();
+    fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{}).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  signup(e) {
+    e.preventDefault();
+    fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+	.catch((error) => {
+	  console.log(error);
+	})
   }
 
   handleChange(e) {
